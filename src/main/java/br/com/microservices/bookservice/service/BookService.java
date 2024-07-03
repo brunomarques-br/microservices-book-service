be08c612a58cc451d050085db62d7cf2d90b59cd
Bruno Marques <brunomcamilo.br@gmail.com>
2024-07-03 01:36:24 -0300

    Flyway disabled because is freemium. Microservice Cambio -> Book Communicating

--- src/main/java/br/com/microservices/bookservice/service/BookService.java
+++ src/main/java/br/com/microservices/bookservice/service/BookService.java
@@ -2,8 +2,12 @@ package br.com.microservices.bookservice.service;
 
 import br.com.microservices.bookservice.model.Book;
 import br.com.microservices.bookservice.repository.BookRepository;
+import br.com.microservices.cambioservice.dto.CambioDTO;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
 
 @Service
 public class BookService {
@@ -19,12 +23,22 @@ public class BookService {
     public Book findBook(Long id, String currency) {
 
         var book = bookRepository.findById(id).orElse(null);
-        if (book != null) {
-            book.setCurrency(currency);
-            book.setEnvironment(environment.getProperty("local.server.port"));
-        } else {
-            throw new RuntimeException("Book not found");
-        }
+        if (book == null) throw new RuntimeException("Book not found");
+
+        HashMap<String, String> params = new HashMap<>();
+        params.put("amount", book.getPrice().toString());
+        params.put("from", "USD");
+        params.put("to", currency);
+
+        var response = new RestTemplate().getForEntity("http://localhost:8000/cambio-service/cambio/{amount}/{from}/{to}",
+                CambioDTO.class, params);
+
+        var cambio = response.getBody();
+        if (cambio == null) throw new RuntimeException("Cambio not found");
+
+        book.setPrice(cambio.getConvertedValue());
+        book.setEnvironment(environment.getProperty("local.server.port"));
+        book.setCurrency(currency);
 
         return book;
     }
