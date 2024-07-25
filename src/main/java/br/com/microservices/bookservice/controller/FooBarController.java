010fae0eeb0b72c189f3d6d14aa546b19f658374
Bruno Marques <brunomcamilo.br@gmail.com>
2024-07-25 15:13:20 -0300

    Knowing annotations and how it's works - Fallback Methods with Resilence4j, Wait Duration and Exponential Backoff

--- src/main/java/br/com/microservices/bookservice/controller/FooBarController.java
+++ src/main/java/br/com/microservices/bookservice/controller/FooBarController.java
@@ -13,15 +13,19 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping("/book-service")
 public class FooBarController {
 
-    private Logger logger = LoggerFactory.getLogger(FooBarController.class);
+    private final Logger logger = LoggerFactory.getLogger(FooBarController.class);
 
     @GetMapping("/foo-bar")
-    @Retry(name = "foo-bar")
+    @Retry(name = "foo-bar", fallbackMethod = "fallbackMethod")
     public String hello() {
         logger.info("Request received in FooBarController");
         var response = new RestTemplate()
                 .getForEntity("http://localhost:8080/foo-bar", String.class);
         return response.getBody();
+    }
+
+    public String fallbackMethod(Exception e) {
+        return "Fallback method called due to exception: " + e.getMessage();
     }
 
 }
