2dc570dfccbce01a8e505201515744ed409b9c8d
Bruno Marques <brunomcamilo.br@gmail.com>
2024-07-26 03:33:15 -0300

    Configured Swagger OpenAPI

--- src/main/java/br/com/microservices/bookservice/controller/BookController.java
+++ src/main/java/br/com/microservices/bookservice/controller/BookController.java
@@ -2,11 +2,14 @@ package br.com.microservices.bookservice.controller;
 
 import br.com.microservices.bookservice.model.Book;
 import br.com.microservices.bookservice.service.BookService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Book Service", description = "Book Service API")
 @RestController
 @RequestMapping("/book-service")
 public class BookController {
@@ -17,6 +20,7 @@ public class BookController {
         this.bookService = bookService;
     }
 
+    @Operation(summary = "Find a book by id and currency")
     @GetMapping(value = "/{id}/{currency}")
     public Book findBook(
             @PathVariable("id") Long id,
--- src/main/java/br/com/microservices/bookservice/controller/FooBarController.java
+++ src/main/java/br/com/microservices/bookservice/controller/FooBarController.java
@@ -1,20 +1,22 @@
 package br.com.microservices.bookservice.controller;
 
 import io.github.resilience4j.bulkhead.annotation.Bulkhead;
-import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@Tag(name = "Foo Bar", description = "FooBar API")
 @RestController
 @RequestMapping("/book-service")
 public class FooBarController {
 
     private final Logger logger = LoggerFactory.getLogger(FooBarController.class);
 
+    @Operation(summary = "Foo Bar API")
     @GetMapping("/foo-bar")
     //@Retry(name = "foo-bar", fallbackMethod = "fallbackMethod")
     // fallback method is called when the circuit is open
