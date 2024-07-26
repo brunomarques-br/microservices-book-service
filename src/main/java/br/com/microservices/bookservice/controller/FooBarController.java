63c9f12f65d185bca80bbdbd7b36913eee9b2539
Bruno Marques <brunomcamilo.br@gmail.com>
2024-07-26 03:08:26 -0300

    Features of RateLimit and BulkHead from Resilience4j

--- src/main/java/br/com/microservices/bookservice/controller/FooBarController.java
+++ src/main/java/br/com/microservices/bookservice/controller/FooBarController.java
@@ -1,13 +1,12 @@
 package br.com.microservices.bookservice.controller;
 
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 
 @RestController
@@ -18,14 +17,17 @@ public class FooBarController {
 
     @GetMapping("/foo-bar")
     //@Retry(name = "foo-bar", fallbackMethod = "fallbackMethod")
-
     // fallback method is called when the circuit is open
-    @CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod")
+    //@CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod")
+    // RateLimiter Defined in application.yml to limit the number of requests 2 per 10 seconds
+    //@RateLimiter(name = "default")
+    @Bulkhead(name = "default")
     public String hello() {
         logger.info("Request received in FooBarController");
-        var response = new RestTemplate()
-                .getForEntity("http://localhost:8080/foo-bar", String.class);
-        return response.getBody();
+//        var response = new RestTemplate()
+//                .getForEntity("http://localhost:8080/foo-bar", String.class);
+//        return response.getBody();
+        return "Hello from book-service";
     }
 
     public String fallbackMethod(Exception e) {
